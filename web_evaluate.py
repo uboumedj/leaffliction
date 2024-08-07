569fd901d7c3011140425b01324590f01882450a
yuxin <yuxin@Workstation.myguest.virtualbox.org>
2024-08-07 20:43:54 +0200

    reworked web_evaluate to consider single model

--- web_evaluate.py
+++ web_evaluate.py
@@ -1,41 +1,35 @@
 import os
 import sys
 import joblib
+import click
 from cli_train import soft_vote, hard_vote, print_accuracy
 # from tensforflow.keras.preprocessing import image_dataset_from_directory temporarily doesn't work ?
 from keras._tf_keras.keras.preprocessing import image_dataset_from_directory
 
+@click.command()
+@click.option('--dataset', default=None, help='Path to the dataset on which evaluation is to be made')
+@click.option('--model_path', default="leaffliction.joblib", help="Path to the trained model to use for prediction")
+def main(dataset, model_path):
+    if os.path.isdir(dataset) is False:
+        return print(f"Argument {dataset} is not a directory")
 
-def main():
-    # argument
-    if len(sys.argv) != 2:
-        return help()
-    if os.path.isdir(sys.argv[1]) is False:
-        return print("Argument {} is not a directory".format(sys.argv[1]))
-
-    fruit = sys.argv[1].split('/', 1)[1]
-    jl_name = os.path.join(sys.argv[1] + '.joblib')
+    model_full_path = os.path.join(model_path)
     data_acc = None
     predictions_validation = []
-    subdirs = [elt for elt in os.listdir(sys.argv[1]) if fruit not in elt]
-    subdirs = sorted(subdirs)
-    models = joblib.load(filename=jl_name)
-    for (model, subdir) in zip(models, subdirs):
-        print(f'evaluating {subdir} model')
-        # data preprocessing
-        data = image_dataset_from_directory(
-            os.path.join(sys.argv[1], subdir),
-            validation_split=0.2,    # 0.8 for training, 0.2 for validation
-            subset="both",
-            shuffle=True,
-            seed=42,
-            image_size=(256, 256),   # 4x less memory and time than (256,256)
-        )
-        validation_data = data[1]
-        data_acc = data[1]
+    model = joblib.load(filename=model_full_path)
+    data = image_dataset_from_directory(
+        os.path.join(dataset),
+        validation_split=0.2,
+        subset="both",
+        shuffle=True,
+        seed=42,
+        image_size=(256, 256),
+        batch_size=2
+    )
+    validation_data = data[1]
+    data_acc = data[1]
 
-        predictions_validation.append(model.predict(validation_data))
-        print()
+    predictions_validation.append(model.predict(validation_data))
 
     soft_vote_predictions = soft_vote(predictions_validation)
     hard_vote_predictions = hard_vote(predictions_validation)
